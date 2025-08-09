420bbef64b4ab86ae02fa684cdedb190c0c74f58
teju81 <teju81@gmail.com>
2025-08-09 16:17:47 +0530

    Create launch_fastlivo2.sh

---
+++ scripts/launch_fastlivo2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Build and source workspace
+source /opt/ros/noetic/setup.bash
+#catkin_make
+source devel/setup.bash
+rm -rf /root/code/datasets/fastlivo2/output/Log
+
+# --- Config ---
+SESSION="fastlivo2_ros_session"
+ROS_BAG=${1:-"/root/code/datasets/fastlivo2/Retail_Street.bag"}
+
+# Command to run fastlivo2 mapping node + RViz + republish
+LAUNCH_FASTLIVO2="roslaunch fast_livo mapping_avia.launch"
+# Command to play rosbag
+PLAY_ROSBAG="rosbag play $ROS_BAG"
+
+# Create tmux session if it doesn't exist
+tmux has-session -t $SESSION 2>/dev/null
+if [ $? != 0 ]; then
+    # Step 1: Create a new tmux session and run rosbag in the first pane
+    tmux new-session -d -s $SESSION -n "ROS"
+
+    # Step 2: Split the window into 4 panes (2x2 grid)
+    tmux split-window -h  # Split horizontally
+
+    tmux send-keys -t 0 "$PLAY_ROSBAG" C-m # left pane
+    tmux send-keys -t 1 "$LAUNCH_FASTLIVO2" C-m # right pane
+
+    # Step 3: Attach to session
+    tmux attach-session -t $SESSION
+else
+    echo "Session $SESSION already exists. Attaching to it."
+    tmux attach-session -t $SESSION
+fi
