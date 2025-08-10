5af2b81792c05549294aa350d1ac911ca5de9860
teju81 <teju81@gmail.com>
2025-08-10 20:37:40 +0530

    Update colmap_output.sh

--- scripts/colmap_output.sh
+++ scripts/colmap_output.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 TARGET_DIRS=(
-    "/root/code/datasets/fastlivo2/output/Log/Colmap/images"
-    "/root/code/datasets/fastlivo2/output/Log/Colmap/sparse/0"
+    "/root/code/datasets/hilti/2022/output/Log/Colmap/images"
+    "/root/code/datasets/hilti/2022/output/Log/Colmap/sparse/0"
 )
+
+# "/root/code/datasets/fastlivo2/output/Log/Colmap/images"
+# "/root/code/datasets/fastlivo2/output/Log/Colmap/sparse/0"
 
 for dir in "${TARGET_DIRS[@]}"; do
     if [ -d "$dir" ]; then
