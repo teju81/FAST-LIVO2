5dda2ac21da9974b8a9b0a26747d43ed2abaaa23
teju81 <teju81@gmail.com>
2025-08-13 12:47:04 +0530

    Update launch_fastlivo2.sh

--- scripts/launch_fastlivo2.sh
+++ scripts/launch_fastlivo2.sh
@@ -9,7 +9,7 @@ rm -rf /root/code/datasets/fastlivo2/output/Log
 # --- Config ---
 SESSION="fastlivo2_ros_session"
 
-CONFIG_FILE="HILTI22.yaml" # avia.yaml or HILTI22.yaml
+CONFIG_FILE="avia.yaml" # avia.yaml or HILTI22.yaml
 
 CONFIG_DIR="/root/code/catkin_ws/src/FAST-LIVO2/config"
 CONFIG_FILE="${CONFIG_DIR}/${CONFIG_FILE}"
@@ -30,12 +30,13 @@ LAUNCH_FASTLIVO2="roslaunch fast_livo $LAUNCH_FILE"
 # Command to play rosbag
 PLAY_ROSBAG="rosbag play $ROS_BAG"
 
-echo "CONFIG_DIR: $CONFIG_DIR"
-echo "CONFIG_FILE: $CONFIG_FILE"
-echo "ROS_BAG: $ROS_BAG"
-echo "OUTPUT_DIR: $OUTPUT_DIR"
-echo "Play ROSBAG Command: $PLAY_ROSBAG"
-echo "LAUNCH_FASTLIVO2 Command: $LAUNCH_FASTLIVO2"
+ECHO_COMMANDS="echo 'CONFIG_DIR: $CONFIG_DIR'; \
+echo 'CONFIG_FILE: $CONFIG_FILE'; \
+echo 'ROS_BAG: $ROS_BAG'; \
+echo 'OUTPUT_DIR: $OUTPUT_DIR'; \
+echo 'Play ROSBAG Command: $PLAY_ROSBAG'; \
+echo 'LAUNCH_FASTLIVO2 Command: $LAUNCH_FASTLIVO2'; \
+bash"
 
 for dir in "${TARGET_DIRS[@]}"; do
     if [ -d "$dir" ]; then
@@ -63,9 +64,13 @@ if [ $? != 0 ]; then
 
     # Step 2: Split the window into 4 panes (2x2 grid)
     tmux split-window -h  # Split horizontally
+    tmux split-window -v  # Split the left pane vertically
+    tmux select-pane -t 0  # Move focus to the first pane (top-left)
+    tmux split-window -v  # Split the right pane vertically
 
-    tmux send-keys -t 0 "$PLAY_ROSBAG" C-m # left pane
-    tmux send-keys -t 1 "$LAUNCH_FASTLIVO2" C-m # right pane
+    tmux send-keys -t 0 "$PLAY_ROSBAG" C-m # Top-left pane
+    tmux send-keys -t 1 "$LAUNCH_FASTLIVO2" C-m # Top-right pane
+    tmux send-keys -t 2 "$ECHO_COMMANDS" C-m # Bottom-left pane
 
     # Step 3: Attach to session
     tmux attach-session -t $SESSION
